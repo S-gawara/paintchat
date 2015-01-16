65b374576e878a6c9356c949888fc09a4e0cc63b
S-gawara <ramunebin64@gmail.com>
2015-01-16 22:01:30 +0900

    port変更

--- app.js
+++ app.js
@@ -35,7 +35,10 @@ app.configure('production', function(){
 app.get('/', routes.index);
 app.get('/index', routes.index);
 
-app.listen(3000, function(){
+// portを追記
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
