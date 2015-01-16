c3590f30e7e824f158d52e6707829dbfaa3db9c0
S-gawara <ramunebin64@gmail.com>
2015-01-16 20:34:38 +0900

    訂正

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express')
   , routes = require('./routes');
 
+
 var app = module.exports = express.createServer();
 // socket.ioのインスタンス
 var io = require('socket.io').listen(app);;
@@ -32,6 +33,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('/index', routes.index);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
